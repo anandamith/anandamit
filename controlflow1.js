d7edbe31e51a8d24488d76410755d471c637bd2b
anandamith <87082096+anandamith@users.noreply.github.com>
2021-07-28 12:08:11 +0530

    Add files via upload

---
+++ controlflow1.js
@@ -0,0 +1,14 @@
+//conditional execution (Conditional statements control behavior in JavaScript and determine whether or not pieces of code can run.)
+// The if/else statement executes a block of code if a specified condition is true. If the condition is false, another block of code can be executed.
+
+// first we have to assign variable to name=Anand
+var name="Anand"
+
+if (name==="Anand") {
+	//if the name is correct then it will alert the given value
+	alert("hi Anand")
+} else {
+//Then if i change name into other name example rihi
+alert("i dont know you")
+
+}
