be4cf9e438244763c33fd1b5457e6388aae754b4
anandamith <87082096+anandamith@users.noreply.github.com>
2021-07-28 13:39:58 +0530

    Add files via upload

---
+++ logical.js
@@ -0,0 +1,22 @@
+//Javascript logical operators
+
+// if we use any of the given name the alert will come
+var name="Joey"
+if (name==="Joey"  || name==="Danny")  {
+	//alert will pop  on screen once everything is equal
+	alert("Hi guys");
+};
+//Apart from this two if we assign name for another word the alert function will not work
+
+
+//&& if we want to add to names the && will help 
+var FirstName="Anand";
+var LastName="Amith"
+if(FirstName==="Anand" && LastName==="Amith") {
+	alert("Hi Anandamith");
+}
+
+//!
+if (!(name==="cat")) {
+	alert("hi cat")
+}
