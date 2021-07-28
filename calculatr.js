9c5d212aec89b505fac5b8c84c7664e5dd039007
anandamith <87082096+anandamith@users.noreply.github.com>
2021-07-28 11:18:30 +0530

    Add files via upload

---
+++ calculatr.js
@@ -0,0 +1,11 @@
+// using prompt and variable i am crating basic calculator!
+
+var first = prompt("enter first number");
+var second=prompt("enter second number");
+//when you mention first will show in string  to change the string into number we have to use number
+first
+var sum = Number(first)-Number(second); 
+//we can change the operations (example we can change - into + or *)
+
+// now i can use the alert and display the sum of first and second number
+alert("The sum is"+sum)
