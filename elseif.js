2bad602f8c021fdb9b4b79fcfacc90e0ee2be4b9
anandamith <87082096+anandamith@users.noreply.github.com>
2021-07-28 13:07:06 +0530

    Add files via upload

---
+++ elseif.js
@@ -0,0 +1,10 @@
+//else if statement we can use this statement when there specific condition is true if its not true we can use it for alternative condition
+ if (name==="John") {
+ 	alert("hi john");
+ } else if (name==="kiddo") {
+ 	alert("hi kiddo");
+ } 
+//if that thing also not working we can use again else
+ else{
+ 	alert ("i dont know you");
+ }
