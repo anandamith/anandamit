9c9e245fbd051785ac38aa19388794dac2398878
anandamith <87082096+anandamith@users.noreply.github.com>
2021-08-02 15:35:04 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,18 @@
+//Object 
+// objects are javascript type.They are collection of  property
+var user={
+	name:"amit",
+	age:22,
+	hobby:"baddmintion",
+    BOY:true  
+  }
+  //Objects are more dynamics they can have property  and values
+
+// we can add properties to and object.
+
+user.favouritemovie = "Thor";
+
+// we can change booleans also
+user.BOY=false;
+
+//object good at  containg something user information
